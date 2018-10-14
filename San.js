3815205b28f98689228185fe7738f904e5bdbc89
martinp1995 <36735023+martinp1995@users.noreply.github.com>
2018-10-14 20:24:27 +0200

    Update San.js

--- San.js
+++ San.js
@@ -134,6 +134,7 @@ client.on('guildMemberAdd', member => {
             .setDescription("Je suis le bot  du serveur **-=D&R=- Village**,\nMon prefix actuel est: "+prefix)
             .addField("Les commandes","`ban`,`kick`,`purge`,`warn`,`seewarns`,`delwarns`")
             .addField("Level","`xpstat`")
+            .addField("Aide","`staff`")
             .setFooter(`demandé par @${message.author.username}`)
             .setTimestamp()
             message.channel.send(help_embed)
